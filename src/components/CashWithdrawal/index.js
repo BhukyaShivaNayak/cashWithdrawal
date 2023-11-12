446eb9bb1bc8da67aa67d248967d515b75bcd621
BhukyaShivaNayak <shivanayak1863@gmail.com>
2023-11-12 15:20:36 +0000

    created Cash Withdrawal using ReactJS

---
+++ src/components/CashWithdrawal/index.js
@@ -0,0 +1,65 @@
+import {Component} from 'react'
+
+import DenominationItem from '../DenominationItem'
+
+import './index.css'
+
+class CashWithdrawal extends Component {
+  state = {total: 2000}
+
+  balanceDeduction = value => {
+    this.setState(prevState => ({
+      total: prevState.total - value,
+    }))
+  }
+
+  render() {
+    const name = 'shiva nayak'
+
+    const slice = name.slice(0, 1)
+
+    const {denominationsList} = this.props
+
+    const {total} = this.state
+
+    return (
+      <div className="bg-container">
+        <div className="card-container">
+          <div className="profile-card">
+            <div className="profile-letter">
+              <p>{slice}</p>
+            </div>
+            <h1 className="profile-name">{name}</h1>
+          </div>
+          <div className="balance-card">
+            <p className="balance-text">Your Balance</p>
+
+            <div>
+              <p className="total-card">{total}</p>
+              <p>In Rupees</p>
+            </div>
+          </div>
+
+          <div className="withdraw-card">
+            <p className="withdraw-text">Withdraw</p>
+
+            <p className="describe">CHOOSE SUM (IN RUPEES)</p>
+            <div>
+              <ul className="card-button">
+                {denominationsList.map(eachValue => (
+                  <DenominationItem
+                    denominationsList={eachValue}
+                    key={eachValue.id}
+                    balanceDeduction={this.balanceDeduction}
+                  />
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CashWithdrawal
---
+++ src/components/DenominationItem/index.js
@@ -0,0 +1,21 @@
+// Write your code here
+const DenominationItem = props => {
+  const {denominationsList, balanceDeduction} = props
+  const {value} = denominationsList
+
+  const totalUpdate = () => {
+    balanceDeduction(value)
+  }
+
+  return (
+    <li className="card-list-container">
+      <div>
+        <button type="button" className="btn" onClick={totalUpdate}>
+          {value}
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default DenominationItem
